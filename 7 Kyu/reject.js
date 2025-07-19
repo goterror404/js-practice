7284ff6787b9e83d7d147c6726bdb5ae355fe2a7
Vishal Yadav <vishalx404@gmail.com>
2025-07-19 09:57:45 +0530

    day 4

--- 7 Kyu/reject.js
+++ 7 Kyu/reject.js
@@ -1,12 +1,6 @@
 // Implement a function
 //  which filters out array values which satisfy the given predicate.
 
-
-// i have to write a function that take an array and predicate 
-
-// if the predicate satisfy the value will be filter out from an array like if the array get 
-// element true it will be filter out form this and the false one will be in it 
-
 function reject(array, predicate) {
     
     let final = []
@@ -23,7 +17,7 @@ function reject(array, predicate) {
 
 
 function reject1(array,predicate){
-    
+
     return array.filter(num => !predicate(num))
 
 }
